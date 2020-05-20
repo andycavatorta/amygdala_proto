0b4e659bc11f02d649327ae4ea1f48913834783a
andycavatorta <andycavatorta@gmail.com>
2020-05-20 15:08:59 -0400

    unifying data types in pubsub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 app_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.split(app_path)[0])
@@ -10,4 +11,8 @@ from thirtybirds3 import thirtybirds
 #ref can be used to access internals when using interactive mode
 tb = thirtybirds.Thirtybirds(settings, app_path)
 
+tb.connection.subscribe_to_topic("test")
 
+while True:
+    tb.connection.send("test","asdf")
+    time.sleep(10)
