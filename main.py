b58f53fd38656e54af9b4ba00f41de8af26f0e53
andycavatorta <andycavatorta@gmail.com>
2020-05-26 18:34:25 -0400

    starting to focus on app level

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-import importlib
-import os
 import socket
-import sys
-import time
+
+import importlib
 import settings
 
-
 role_module = importlib.import_module('roles.{}.{}'.format(settings.Roles.hosts[socket.gethostname()],'main'))
-
-"""
-tb.subscribe_to_topic("test")
-
-while True:
-    #print(tb.connection.check_connections())
-    #time.sleep(2)
-    #tb.connection.subscribe_to_topic()
-    time.sleep(2)
-    
-    #tb.connection.send("test")
-    tb.publish("test",{"a":1,"b":2})
-"""
