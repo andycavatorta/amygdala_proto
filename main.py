ed8f83a9c023913562631c11e4148d899e3befd8
andycavatorta <andycavatorta@gmail.com>
2020-05-26 18:26:45 -0400

    starting to focus on app level

--- main.py
+++ main.py
@@ -1,32 +1,17 @@
+import importlib
 import os
+import socket
 import sys
 import time
-
-app_path = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.split(app_path)[0])
-
 import settings
-from thirtybirds3 import thirtybirds
 
-def network_message_handler(topic, message):
-    print("network_message_handler",topic, message)
 
-def exception_handler(exception):
-    print("exception_handler",exception)
+role_module = importlib.import_module('roles.{}.{}'.format(settings.Roles.hosts[socket.gethostname()],'main'))
 
-def network_status_change_handler(online_status):
-    print("network_status_change_handler",online_status)
 
-#ref can be used to access internals when using interactive mode
-tb = thirtybirds.Thirtybirds(
-    settings, 
-    app_path,
-    network_message_handler,
-    network_status_change_handler,
-    exception_handler
-)
-tb.init()
 
+
+"""
 tb.subscribe_to_topic("test")
 
 while True:
@@ -37,3 +22,4 @@ while True:
     
     #tb.connection.send("test")
     tb.publish("test",{"a":1,"b":2})
+"""
