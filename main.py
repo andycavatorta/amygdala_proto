4c9865c293a3cc096046865957a50b58ea452244
andycavatorta <andycavatorta@gmail.com>
2020-05-26 18:31:09 -0400

    starting to focus on app level

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ import settings
 
 role_module = importlib.import_module('roles.{}.{}'.format(settings.Roles.hosts[socket.gethostname()],'main'))
 
-
-
-
 """
 tb.subscribe_to_topic("test")
 
---
+++ roles/controller/main.py
@@ -0,0 +1,31 @@
+import importlib
+import os
+import sys
+import time
+
+
+app_path = os.path.dirname((os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+print(app_path)
+sys.path.append(os.path.split(app_path)[0])
+
+import settings
+from thirtybirds3 import thirtybirds
+
+def network_message_handler(topic, message):
+    print("network_message_handler",topic, message)
+
+def exception_handler(exception):
+    print("exception_handler",exception)
+
+def network_status_change_handler(online_status):
+    print("network_status_change_handler",online_status)
+
+tb = thirtybirds.Thirtybirds(
+    settings, 
+    app_path,
+    network_message_handler,
+    network_status_change_handler,
+    exception_handler
+)
+#tb.init()
+
