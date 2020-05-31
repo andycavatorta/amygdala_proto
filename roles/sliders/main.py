110cc855c67d0b75ecd670bcdfa8ef9bf0e4c45e
andycavatorta <andycavatorta@gmail.com>
2020-05-30 22:30:31 -0400

    testing

--- roles/sliders/main.py
+++ roles/sliders/main.py
@@ -58,7 +58,7 @@ controllers = roboteq_command_wrapper.Controllers(
 
 time.sleep(10)
 for board_name in controllers.boards:
-    controllers.boards[board_name].read_mixed_mode()
+    print(controllers.boards[board_name])
 
 """
 controllers.motors["pitch_slider"].set_encoder_counter(00)
