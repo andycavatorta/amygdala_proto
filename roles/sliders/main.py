804718affda6bf6ff2e31239db4cdf67dd91c66e
andycavatorta <andycavatorta@gmail.com>
2020-06-01 16:35:46 -0400

    testing

--- roles/sliders/main.py
+++ roles/sliders/main.py
@@ -58,7 +58,7 @@ controllers = roboteq_command_wrapper.Controllers(
 
 
 
-def do_tests(){
+def do_tests():
     controllers.motors["pitch_slider"].go_to_speed_or_relative_position(200)
     controllers.motors["bow_position_slider"].go_to_speed_or_relative_position(200)
     controllers.motors["bow_height"].go_to_speed_or_relative_position(200)
@@ -79,7 +79,7 @@ def do_tests(){
     controllers.motors["bow_height"].go_to_speed_or_relative_position(0)
     controllers.motors["bow_rotation"].go_to_speed_or_relative_position(0)
     time.sleep(5)
-}
+
 #time.sleep(10)
 #print(controllers.boards)
 #for board_name in controllers.boards:
