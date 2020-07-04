0433f32022f20a3fcf1b7af725df3b10fcff3a5b
Andy Cavatorta <hellyeah@media.mit.edu>
2020-07-04 18:10:38 -0400

    testing bow position

--- roles/horsewheel/main.py
+++ roles/horsewheel/main.py
@@ -61,7 +61,7 @@ controllers = roboteq_command_wrapper.Controllers(
 
 controllers.motors["bow_rotation"].get_encoder_counter_absolute(True)
 controllers.motors["bow_rotation"].go_to_speed_or_relative_position(7)
-time.sleep(20)
+time.sleep(30)
 controllers.motors["bow_rotation"].go_to_speed_or_relative_position(0)
 controllers.motors["bow_rotation"].go_to_speed_or_relative_position(0)
 controllers.motors["bow_rotation"].go_to_speed_or_relative_position(0)
--- settings.py
+++ settings.py
@@ -107,7 +107,7 @@ class Roboteq:
             "operating_mode":1, #0: Open-loop,1: Closed-loop speed,2: Closed-loop position relative,3: Closed-loop count position,4: Closed-loop position tracking,5: Torque,6: Closed-loop speed position
             "pid_differential_gain":1, # 0-255
             "pid_integral_gain":100, # 0-255
-            "pid_proportional_gain":50, # 0-255
+            "pid_proportional_gain":100, # 0-255
             "encoder_ppr_value":4000,
             # todo: more variable names will be added as needed
         }
