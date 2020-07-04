121d886c162ede43e64a096451af49f86c843cd3
Andy Cavatorta <hellyeah@media.mit.edu>
2020-07-04 16:59:42 -0400

    testing bow height

--- roles/horsewheel/main.py
+++ roles/horsewheel/main.py
@@ -58,14 +58,14 @@ controllers = roboteq_command_wrapper.Controllers(
     }
 )
 
-"""
+
 controllers.motors["bow_rotation"].get_encoder_counter_absolute(True)
-controllers.motors["bow_rotation"].go_to_speed_or_relative_position(4)
+controllers.motors["bow_rotation"].go_to_speed_or_relative_position(0)
 time.sleep(10)
 controllers.motors["bow_rotation"].go_to_speed_or_relative_position(0)
 controllers.motors["bow_rotation"].get_encoder_counter_absolute(True)
 #time.sleep(2)
-"""
+
 """
 #controllers.motors["bow_height"].get_encoder_counter_absolute(True)
 #controllers.motors["bow_height"].go_to_speed_or_relative_position(10)
