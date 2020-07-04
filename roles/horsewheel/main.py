dc20a72316c926647c9f471c56869d1f6842acc2
Andy Cavatorta <hellyeah@media.mit.edu>
2020-07-04 16:36:51 -0400

    testing bow height

--- roles/horsewheel/main.py
+++ roles/horsewheel/main.py
@@ -64,12 +64,16 @@ controllers.motors["bow_rotation"].go_to_speed_or_relative_position(2)
 time.sleep(20)
 controllers.motors["bow_rotation"].go_to_speed_or_relative_position(0)
 controllers.motors["bow_rotation"].get_encoder_counter_absolute(True)
-time.sleep(2)
-controllers.motors["bow_height"].get_encoder_counter_absolute(True)
-controllers.motors["bow_height"].go_to_speed_or_relative_position(3)
-time.sleep(2)
-controllers.motors["bow_height"].go_to_speed_or_relative_position(0)
-controllers.motors["bow_height"].get_encoder_counter_absolute(True)
+#time.sleep(2)
+#controllers.motors["bow_height"].get_encoder_counter_absolute(True)
+#controllers.motors["bow_height"].go_to_speed_or_relative_position(3)
+#time.sleep(2)
+#controllers.motors["bow_height"].go_to_speed_or_relative_position(0)
+#controllers.motors["bow_height"].get_encoder_counter_absolute(True)
+
+controllers.motors["bow_rotation"].set_operating_mode(0)
+controllers.motors["bow_height"].set_operating_mode(0)
+
 
 
 """
