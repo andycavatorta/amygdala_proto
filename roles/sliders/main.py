8f54717f34716fa4d87122ae89f706f1799d0977
Andy Cavatorta <hellyeah@media.mit.edu>
2020-07-04 17:57:27 -0400

    testing bow position

--- roles/sliders/main.py
+++ roles/sliders/main.py
@@ -59,7 +59,7 @@ controllers = roboteq_command_wrapper.Controllers(
 )
 
 controllers.macros["bow_position_slider"].add_to_queue("go_to_limit_switch")
-controllers.macros["pitch_slider"].add_to_queue("go_to_absolute_position", {"position":500000, "speed":100})
+controllers.macros["bow_position_slider"].add_to_queue("go_to_absolute_position", {"position":500000, "speed":100})
 #controllers.macros["pitch_slider"].add_to_queue("oscillate", {"distance":500,"frequency":0.5,"duration":10})
 
 
