1d08107fcdb119430eb7056ec189012e1fee4a66
Andy Cavatorta <hellyeah@media.mit.edu>
2020-07-06 15:11:52 -0400

    testing bow lifter

--- roles/horsewheel/main.py
+++ roles/horsewheel/main.py
@@ -74,7 +74,7 @@ controllers.motors["bow_rotation"].get_encoder_counter_absolute(True)
 """
 controllers.motors["bow_height"].get_encoder_counter_absolute(True)
 controllers.motors["bow_height"].go_to_speed_or_relative_position(1000)
-time.sleep(2)
+time.sleep(5)
 #controllers.motors["bow_height"].go_to_speed_or_relative_position(0)
 #controllers.motors["bow_height"].get_encoder_counter_absolute(True)
 #controllers.motors["bow_height"].go_to_speed_or_relative_position(0)
