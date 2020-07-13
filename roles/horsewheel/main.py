04531d1288ba3e5bcfa9e874b7e8df5fbe121d0f
Andy Cavatorta <hellyeah@media.mit.edu>
2020-07-13 16:20:47 -0400

    testing limit switch

--- roles/horsewheel/main.py
+++ roles/horsewheel/main.py
@@ -83,7 +83,7 @@ class Main(threading.Thread):
                     self.controllers.macros["bow_rotation"].set_speed(int(message))
                 if topic == b"horsewheel_lifter_position":
                     print("________", int(message))
-                    self.controllers.motors["bow_height"].go_to_absolute_position(int(-message))
+                    self.controllers.motors["bow_height"].go_to_relative_position(int(-message))
                     #self.controllers.macros["bow_height"].go_to_absolute_position({"position":int(-message), "speed":50})
             except Exception as e:
                 exc_type, exc_value, exc_traceback = sys.exc_info()
